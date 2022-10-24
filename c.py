9363379f8805ae81a916a62a08774f1995564918
Mohdhaseeb26 <116086121+Mohdhaseeb26@users.noreply.github.com>
2022-10-24 08:41:41 +0530

    Loops,functions.py
    loop's and function's

---
+++ c.py
@@ -0,0 +1,16 @@
+# assignment1
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+  
+  fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+  
+  def my_function(fname):
+  print(fname + " Refsnes")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
